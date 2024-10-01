c662bc3c6b15735243e5c976cf71b35786865c5c
Ali Mansoori <71168717+alimansoo@users.noreply.github.com>
2024-10-01 20:48:13 +0330

    add logo

--- src/shared/navbar.jsx
+++ src/shared/navbar.jsx
@@ -1,19 +1,24 @@
 import { AppBar, Toolbar, Typography, Button, Container } from "@mui/material";
 
+import Logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <AppBar position="static" color="default">
       <Container maxWidth="lg">
         <Toolbar className="!pl-0 !pr-0">
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            SWAP MART
+            <img src={Logo} className="h-9" />
           </Typography>
           <Button color="inherit" href="/">
             Home
           </Button>
-          <Button color="inherit" href="/about">
-            About
-          </Button>
+          <Link to={`/new`}>
+            <Button variant="contained">
+              Create Post
+            </Button>
+          </Link>
         </Toolbar>
       </Container>
     </AppBar>
