0c8d22322d4008be69e00ead63d4486d4287ef06
Ali Mansoori <71168717+ali-mansi-dev82@users.noreply.github.com>
2024-11-23 00:23:49 +0330

    fix single post page

--- src/pages/single_post/index.jsx
+++ src/pages/single_post/index.jsx
@@ -1,21 +1,91 @@
 import { useParams } from "react-router-dom";
-import { FindPostbySlugFn } from "../../queries/post";
+import PropTypes from "prop-types";
 import { useQuery } from "react-query";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
+import { FindPostbySlugFn } from "../../queries/post";
 import Mobile from "./mobile";
 import Desktop from "./desktop";
-import PropTypes from "prop-types";
+import Supabase from "../../lib/helper/ClientSupabase";
 
 const SinglePost = ({ isMobile }) => {
   const { slug } = useParams();
-  const { isLoading, data } = useQuery(
-    "singlepost",
-    FindPostbySlugFn.bind(this, slug)
+  const { is_authed, user_info } = useSelector((redux) => redux.auth);
+  const [isBookmark, setIsBookmark] = useState("loading");
+
+  const { isFetching, data } = useQuery(
+    ["post_data", slug],
+    () => FindPostbySlugFn(slug),
+    { keepPreviousData: false }
   );
 
+  //viewed quer
+  useEffect(() => {
+    (async function () {
+      if (is_authed && data?.id && user_info?.id) {
+        const { data: viewed_data } = await Supabase.from("sw_post_viewed")
+          .select("*")
+          .eq("post", data?.id)
+          .eq("user", user_info?.id);
+
+        if (!viewed_data.length)
+          await Supabase.from("sw_post_viewed")
+            .insert([{ post: data?.id }])
+            .select();
+        else
+          await Supabase.from("sw_post_viewed")
+            .update({ created_at: new Date().toISOString() })
+            .eq("post", data?.id)
+            .eq("user", user_info?.id);
+      }
+    })();
+  }, [data, is_authed, user_info]);
+
+  //bookmark queries
+  useEffect(() => {
+    (async function () {
+      if (is_authed && data?.id && user_info?.id) {
+        const { data: saved_data } = await Supabase.from("sw_post_saved")
+          .select("*")
+          .eq("post", data?.id)
+          .eq("user", user_info?.id);
+
+        setIsBookmark(saved_data.length > 0);
+      } else setIsBookmark(false);
+    })();
+  }, [data, is_authed, user_info]);
+
+  const onSaveBtnClick = async () => {
+    if (isBookmark === true) {
+      const { error } = await Supabase.from("sw_post_saved")
+        .delete()
+        .eq("post", data?.id)
+        .eq("user", user_info?.id);
+
+      !error && setIsBookmark(false);
+    } else if (isBookmark === false) {
+      const { error } = await Supabase.from("sw_post_saved")
+        .insert([{ post: data?.id }])
+        .select();
+      !error && setIsBookmark(true);
+    }
+  };
+
   return isMobile ? (
-    <Mobile isLoading={isLoading} data={data} />
+    <Mobile
+      isLoading={isFetching}
+      data={data}
+      onSaveBtnClick={onSaveBtnClick}
+      isBookmark={isBookmark}
+    />
   ) : (
-    <Desktop isLoading={isLoading} data={data} />
+    <Desktop
+      isLoading={isFetching}
+      data={data}
+      onSaveBtnClick={onSaveBtnClick}
+      isBookmark={isBookmark}
+    />
   );
 };
 SinglePost.propTypes = { isMobile: PropTypes.bool };
