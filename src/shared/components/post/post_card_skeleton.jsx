5a46432567a3402dd4055cd8e41894f3ea5baa34
Ali Mansoori <71168717+ali-mansi-dev82@users.noreply.github.com>
2024-11-23 12:42:59 +0330

    add filter by category slug

--- src/shared/components/post/post_card_skeleton.jsx
+++ src/shared/components/post/post_card_skeleton.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@mui/material";
 
 const PostCardSkeleton = () => {
   return (
-    <div className="flex p-4 border border-gray-200 justify-between rounded-md gap-2 cursor-pointer min-w-[310px]">
+    <div className="flex p-4 border border-gray-200 justify-between rounded-md gap-2 cursor-pointer w-full">
       <div className="flex flex-col h-full w-1/2 gap-2">
         <Skeleton variant="rounded" className="w-full" height={30} />
         <Skeleton variant="rounded" className="w-full" height={20} />
